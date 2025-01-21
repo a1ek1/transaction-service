1045ed2698f2fec173a158fba4f3c7fde284231a
Oleg Semchenko <ogresemchenko326@gmail.com>
2025-01-21 23:24:27 +0700

    Implement transaction creation in SendMoney func

--- internal/domain/service/wallet_service.go
+++ internal/domain/service/wallet_service.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/google/uuid"
+	"time"
+	"transaction-service/internal/domain/model"
 
 	"transaction-service/internal/domain/repository"
 )
@@ -14,21 +16,22 @@ type WalletService interface {
 }
 
 type walletService struct {
-	repository.WalletRepository
+	walletRepo      repository.WalletRepository
+	transactionRepo repository.TransactionRepository
 }
 
-func NewWalletService(repository repository.WalletRepository) WalletService {
-	return &walletService{repository}
+func NewWalletService(walletRepo repository.WalletRepository, transactionRepo repository.TransactionRepository) WalletService {
+	return &walletService{walletRepo: walletRepo, transactionRepo: transactionRepo}
 }
 
 func (w walletService) SendMoney(ctx context.Context, fromID, toID uuid.UUID, amount int) error {
-	tx, err := w.WalletRepository.BeginTransaction()
+	tx, err := w.walletRepo.BeginTransaction()
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
 
-	senderWallet, err := w.WalletRepository.FetchByID(ctx, fromID)
+	senderWallet, err := w.walletRepo.FetchByID(ctx, fromID)
 	if err != nil {
 		return err
 	}
@@ -38,20 +41,33 @@ func (w walletService) SendMoney(ctx context.Context, fromID, toID uuid.UUID, am
 	}
 
 	senderWallet.Amount -= amount
-	_, err = w.WalletRepository.Update(ctx, senderWallet)
+	_, err = w.walletRepo.Update(ctx, senderWallet)
 	if err != nil {
 		return err
 	}
 
-	receiverWallet, err := w.WalletRepository.FetchByID(ctx, toID)
+	receiverWallet, err := w.walletRepo.FetchByID(ctx, toID)
 	if err != nil {
 		return err
 	}
 
 	receiverWallet.Amount += amount
-	_, err = w.WalletRepository.Update(ctx, receiverWallet)
+	_, err = w.walletRepo.Update(ctx, receiverWallet)
 	if err != nil {
 		return err
+	}
+
+	transaction := &model.Transaction{
+		ID:        uuid.New(),
+		From:      fromID.String(),
+		To:        toID.String(),
+		Amount:    amount,
+		CreatedAt: time.Now(),
+	}
+
+	_, err = w.transactionRepo.Create(ctx, transaction)
+	if err != nil {
+		return fmt.Errorf("failed to create transaction: %w", err)
 	}
 
 	if err := tx.Commit(); err != nil {
@@ -62,7 +78,7 @@ func (w walletService) SendMoney(ctx context.Context, fromID, toID uuid.UUID, am
 }
 
 func (w walletService) GetBalance(ctx context.Context, id uuid.UUID) (amount int, err error) {
-	wallet, err := w.WalletRepository.FetchByID(ctx, id)
+	wallet, err := w.walletRepo.FetchByID(ctx, id)
 	if err != nil {
 		return 0, fmt.Errorf("failed to fetch wallet: %w", err)
 	}
